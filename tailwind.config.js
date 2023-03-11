dc3c9c959b4c64f1b73b923ed79a62fd5ed4702c
Kendra-Petit <46709079+Kendra-Petit@users.noreply.github.com>
2023-03-11 15:00:23 +0100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    'src/App.js',
+    'src/**/*.js',
+    'src/components/**/*.js',
+    'src/screens/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
